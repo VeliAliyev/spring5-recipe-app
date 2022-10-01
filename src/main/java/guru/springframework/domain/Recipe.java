fd5c8f0ac67871a4e9114272af2a8c9cbec3b3d8
VeliAliyev <veli_aliyev@outlook.com>
2022-10-01 19:19:03 +0300

    Recipe entity

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,33 @@
+package guru.springframework.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+}
