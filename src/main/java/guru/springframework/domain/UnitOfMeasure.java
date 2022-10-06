1b6f836168bb5c93c3171ba9679c2246b9693ede
VeliAliyev <veli_aliyev@outlook.com>
2022-10-06 16:05:36 +0300

    UnitOfMeasure entity

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -5,26 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
 @Builder
 @Data
-@Entity
-public class Ingredient {
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-    private BigDecimal amount;
-
-    @ManyToOne
-    private Recipe recipe;
-
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure uom;
 
 }
