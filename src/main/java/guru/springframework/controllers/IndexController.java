ae248870619d4d5a4b5fd4831b2aa74207195784
VeliAliyev <veli_aliyev@outlook.com>
2022-10-06 18:09:45 +0300

    IndexController updated

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,31 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.service.RecipeService;
+import guru.springframework.service.RecipeServiceImpl;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@AllArgsConstructor
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final RecipeService recipeService;
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
-
 }
