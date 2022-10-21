cd92fb8de106c0672d50b2d600a8f8d3120a0d90
VeliAliyev <veli_aliyev@outlook.com>
2022-10-21 17:51:44 +0300

    index page updated

--- src/main/java/guru/springframework/repositories/RecipeRepository.java
+++ src/main/java/guru/springframework/repositories/RecipeRepository.java
@@ -1,7 +1,6 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Recipe;
-import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
